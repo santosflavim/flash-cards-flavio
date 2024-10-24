4f2e1d1e2d256f4ca8910a543cfb4480a80ea17c
santosflavim <145395434+santosflavim@users.noreply.github.com>
2024-10-24 14:56:46 -0300

    24/10

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,48 @@
 criaCartao(
-    'categoria'
-    'pergunta'
-    'resposta'
+    'geral'
+    'Quanto tempo a luz do Sol demora para chegar à Terra?
+    ''
+a) 12 minutos.
+b) 1 dia.
+c) 12 horas.
+d) 8 minutos..
+e) 12 segundos'.
+    
+
 )
 
 criaCartao(
-    'categoria'
-    'pergunta'
-    'resposta'
+    'portugues'
+    Como é a conjugação do verbo caber na 1.ª pessoa do singular do presente do indicativo?
+
+a) Eu caibo..
+b) Ele cabe.
+c) Que eu caiba.
+d) Eu cabo.
+e) Nenhuma das alternativas.
+    
 )
 
 criaCartao(
-    'categoria'
-    'pergunta'
-    'resposta'
+    'ciencias'
+    Como se chamam os vasos que transportam sangue do coração para a periferia do corpo?
+a) veias.
+b) átrios.
+c) ventrículos.
+d) artérias..
+e) aurículos.
+
+
+    
 )
 
 criaCartao(
-    'categoria'
-    'pergunta'
-    'resposta'
+    'historia'
+    Em que ano e quem foi eleito o primeiro presidente do Brasil?
+    a) 1891, Deodoro da Fonseca..
+    b) 1890, Floriano Peixoto.
+    c) 1889, Hermes da Fonseca.
+    d) 1930, Getúlio Vargas.
+    e) 1822, Deodoro da Fonseca.  
+    
 )
