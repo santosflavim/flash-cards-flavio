6732a731344895d74e4f2f48cd80d417408471f2
santosflavim <145395434+santosflavim@users.noreply.github.com>
2024-10-24 14:33:35 -0300

    24/10/2024

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'categoria'
+    'pergunta'
+    'resposta'
+)
+
+criaCartao(
+    'categoria'
+    'pergunta'
+    'resposta'
+)
+
+criaCartao(
+    'categoria'
+    'pergunta'
+    'resposta'
+)
+
+criaCartao(
+    'categoria'
+    'pergunta'
+    'resposta'
+)
