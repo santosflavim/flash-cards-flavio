d4b0faddecc37b2a95e91561cee74d97976b370f
santosflavim <145395434+santosflavim@users.noreply.github.com>
2024-11-14 14:57:06 -0300

    14/11

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ function criaCartao(categoria, pergunta, resposta){
 let cartao = document.createElement('article');
 cartao.className = 'cartao';
 
-cartao.innerHTML = 
-                `<div class="cartao__conteudo">
+cartao.innerHTML = `
+                <div class="cartao__conteudo">
                     <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
                         <p>${pergunta}?</p>
@@ -15,7 +15,7 @@ cartao.innerHTML =
                 </div>
                 `
 
-    let respostasEstavisivel= false
+    let respostaEstavisivel= false;
 
  function viracartao(){
      respostasEstavisivel = !respostasEstavisivel;
